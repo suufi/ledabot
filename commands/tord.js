4b7daf600e3bd9fa90fe751b715fa1139938a831
Unknown <qxest@reqxest.com>
2020-06-13 18:37:59 -0400

    Allow random selection for players who haven't gone

--- commands/tord.js
+++ commands/tord.js
@@ -9,7 +9,7 @@ const adapter = new FileSync('database.json')
 const db = low(adapter)
 
 if (!db.get('tord_players').value()) {
-    db.defaults({ tord_players: [] }).write()
+    db.defaults({ tord_players: [], played: [] }).write()
 }
 
 class TruthOrDareCommand extends Command {
@@ -26,7 +26,7 @@ class TruthOrDareCommand extends Command {
     }
 
     async exec(message, args) {
-        if (['join', 'next', 'leave', 'clear', 'players', 'ls', 'n', 'cf', 'coin', 'coinflip'].includes(args.action)) {
+        if (['join', 'next', 'leave', 'clear', 'players', 'ls', 'n', 'cf', 'coin', 'coinflip', 'clp', 'clear-played', 'random', 'r', 'rp'].includes(args.action)) {
             if (args.action === 'join') {
                 console.log(db.get('tord_players').value())
                 if (db.get('tord_players').value().includes(message.author.id)) return message.reply("You are already in the game. :clown:")
@@ -36,10 +36,11 @@ class TruthOrDareCommand extends Command {
                 await message.reply("You have been added to the current game. :white_check_mark:")
             } else if (args.action === 'players' || args.action === 'ls') {
                 const players = db.get('tord_players').value()
+                const playersWhoHaveGone = db.get('played').value()
 
                 const embed = new Discord.MessageEmbed()
                     .setTitle("LeDaddy TorD")
-                    .addField("Current Players", (players.length > 0 ? `- ${players.map(player => `<@${player}>`).join('\n- ')}` : 'none'))
+                    .addField("Current Players", (players.length > 0 ? `- ${players.map(player => `<@${player}> ${playersWhoHaveGone.includes(player) ? '(HP)' : '' }`).join('\n- ')}` : 'none'))
 
                 await message.reply({ embed })
             } else if (args.action === 'leave') {
@@ -50,18 +51,47 @@ class TruthOrDareCommand extends Command {
             } else if (args.action === 'clear') {
                 if (message.member.roles.cache.has(process.env.ADMIN_ROLE_ID)) {
                     db.set('tord_players', []).write()
+                    db.set('played', []).write()
                     await message.reply("Cleared the TorD player list. :white_check_mark:")
                 }
             } else if (args.action === 'next' || args.action === 'n') {
                 let players = db.get('tord_players').value()
+                let playersWhoHaveGone = db.get('played').value()
+                players = players.filter(player => !playersWhoHaveGone.includes(player))
                 if (players.length > 0) {
-                    await message.reply(`The next player is: <@${players[Math.floor(Math.random()*players.length)]}>`)
+                    let selectedPlayer = players[Math.floor(Math.random() * players.length)]
+                    await message.reply(`The next player is: <@${selectedPlayer}>`)
+                    db.get('played')
+                      .push(selectedPlayer)
+                      .write()
                 } else {
-                    await message.reply("Looks like you're playing with ghosts. :ghost: ")
+                    await message.reply("Looks like you're playing with ghosts. :ghost: It's either that or everyone has already had 1 turn.")
+                }
+            } else if (['random', 'r', 'rp'].includes(args.action)) {
+                let players = db.get('tord_players').value()
+                if (players.length > 0) {
+                    await message.reply(`The next player is: <@${players[Math.floor(Math.random() * players.length)]}>`)
+                } else {
+                    await message.reply("Looks like you're playing with ghosts. :ghost: It's either that or everyone has already had 1 turn.")
+                }
+            } else if (['clear-played', 'clp'].includes(args.action)) {
+                if (message.member.roles.cache.has(process.env.ADMIN_ROLE_ID)) {
+                    db.set('played', []).write()
+                    await message.reply("Cleared the TorD played list. :white_check_mark:")
+                }
+            } else if (['remove', 'r'].includes(args.action.split(' ')[0])) {
+                if (message.member.roles.cache.has(process.env.ADMIN_ROLE_ID)) {
+                    const playerToRemove = args.action.split(' ')[1]
+                    if (playerToRemove) {
+                        let players = db.get('tord_players').value()
+                        players = players.filter(player => player !== playerToRemove)
+                        db.set('tord_players', players).write()
+                        await message.reply(`Removed ${playerToRemove} :white_check_mark:`)
+                    }
                 }
             } else if (['cf', 'coin', 'coinflip'].includes(args.action)) {
                 let result = Math.random()
-                await message.reply(result > 0.5 ? "The coin landed on: heads" : "The coin landed on: tails")
+                await message.reply(result >= 0.5 ? "The coin landed on: heads" : "The coin landed on: tails")
             }
         }
     }
